c694d7b3129b5719f5cc9b196cbbc39375babccb
lqw7 <ql1@st-andrews.ac.uk>
2021-07-02 18:59:25 +0100

    12. Resolving cross-domain issues

--- web/src/views/search.vue
+++ web/src/views/search.vue
@@ -12,6 +12,36 @@
             @search="onSearch"
         />
       </div>
+      <div id="test1">
+        <p>test</p>
+      </div>
+      <div id="select1">
+        <a-select
+            v-model:value="value"
+            mode="tags"
+            style="width: 100%"
+            placeholder="Tags Mode"
+            @change="handleChange"
+        >
+          <a-select-option v-for="i in 25" :key="(i + 9).toString(36) + i">
+            {{ (i + 9).toString(36) + i }}
+          </a-select-option>
+        </a-select>
+      </div>
+      <div id="select2">
+        <a-select
+            mode="multiple"
+            :size="size"
+            placeholder="Please select"
+            v-model:value="value2"
+            style="width: 200px"
+            @popupScroll="popupScroll"
+        >
+          <a-select-option v-for="i in 25" :key="(i + 9).toString(36) + i">
+            {{ (i + 9).toString(36) + i }}
+          </a-select-option>
+        </a-select>
+      </div>
   </a-layout>
 </template>
 
@@ -44,6 +74,25 @@ export default defineComponent({
 #search{
   height: 100px;
   width:650px;
+  margin: 0 auto;
+}
+#test1 p{
+  font-size: 25px;
+  font-weight: bold;
+  text-align: center;
+  font-family: "Microsoft YaHei";
+  color: #141414;
+  margin-top: 10px;
+}
+#select1{
+  height: 120px;
+  width:650px;
+  margin: 0 auto;
+}
+
+#select2{
+  height: 120px;
+  width:550px;
   margin: 0 auto;
 }
 </style>
